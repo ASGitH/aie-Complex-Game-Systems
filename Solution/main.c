02c12296b158fdb3b3c2d811a500ff3fcbe2db36
Alexis Serna <7319aserna@gmail.com>
2019-12-05 17:55:05 -0800

    Scrollable background is halfway implemented, just got to figure out how when the player moves back left, to use the previous tiles

--- Solution/main.c
+++ Solution/main.c
@@ -4,53 +4,41 @@
 #include "map_Configuration.c"
 
 void main(){
-    // 0, 0, 0, 1, 0 = set_Map(1, 5);
-    set_bkg_data(0, 2, dungeon_Map_Sprites);
-
-    instantiate_Player(16, 24);
+    //move_Background();
     set_Map(2, 2);
 
-    SHOW_BKG;
-    SHOW_SPRITES; 
-
     while(1){
-        // switch(joypad()){
-        //     case J_DOWN:
-        //     current_Position_On_Map = convert_Map_Position(position_On_Map);
-        //     change_Room(0);
-        //     break;
-        //     case J_LEFT:
-        //     current_Position_On_Map = convert_Map_Position(position_On_Map);
-        //     change_Room(1);
-        //     break;
-        //     case J_RIGHT:
-        //     current_Position_On_Map = convert_Map_Position(position_On_Map);
-        //     change_Room(2);
-        //     break;
-        //     case J_UP:
-        //     current_Position_On_Map = convert_Map_Position(position_On_Map);
-        //     change_Room(3);
-        //     break;
-        // }
+        wait_vbl_done();
 
         switch(joypad()){
-            case J_DOWN:
-                move_Player(0);
-                move_Background(map_Array[map_Index_Array[current_Position_On_Map]], 0, 5, player_Position);
-            break;
             case J_LEFT:
-                move_Player(1);
-                move_Background(map_Array[map_Index_Array[current_Position_On_Map]], 1, 5, player_Position);
+                if(scroll_X != 0){ scroll_bkg( -8, 0 ); tile_Counter--; }
             break;
             case J_RIGHT:
-                move_Player(2);
-                move_Background(map_Array[map_Index_Array[current_Position_On_Map]], 2, 5, player_Position);            
+                if(scroll_X < 20 - 20){
+                    scroll_bkg(8, 0);
+                    tile_Counter++;
+                }            
             break;
-            case J_UP:
-                move_Player(4);
-                move_Background(map_Array[map_Index_Array[current_Position_On_Map]], 4, 5, player_Position);            
-            break;
+            // case J_A:
+            //     set_bkg_tiles(1, 1, 1, 1, &(blank_Dungeon_Room));
+            // break;
         }
+
+        // Tile_Counter == 8 (8 is how many times it would take before the screen would update and refresh)
+        if(tile_Counter == /*8*/ 1){
+            scroll_X++;
+            tile_Counter = 0;
+
+            counter = scroll_X + 21;
+            temporary_B = counter % 32;
+
+            for(temporary_A = 0; temporary_A != 18; temporary_A++){
+                set_bkg_tiles(temporary_B, temporary_A, 1, 1, &(blank_Dungeon_Room));
+                counter = counter + 20;
+            }
+        }
+
         delay(99);
     }
 }
